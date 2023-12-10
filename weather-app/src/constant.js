acddf03a16e31292f788255d48ef6796d49fa9b2
Rishabh Kumar <rishabhkumar@Rishabhs-MacBook-Pro.local>
2023-12-10 18:13:55 +0300

    ENV changes

--- weather-app/src/constant.js
+++ weather-app/src/constant.js
@@ -1,4 +1,4 @@
-export const API_KEY = "ADD OPEN WEATHER API KEY";
+export const API_KEY = import.meta.env.VITE_API_KEY;
 export const UNIT = "metric";
 export const API_URL = "https://api.openweathermap.org/data/2.5/weather";
 export const IMG_URL = "https://openweathermap.org/img/wn";
